8e445830a5698853c554f7229b8c84e265ae6db8
amitambig <49871474+amitambig@users.noreply.github.com>
2024-08-19 11:33:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-vpc-demo-project-tfstate"
+    bucket = "my-vpc-demo-project-tfstate-state-file "
     key = "keys/terraform.tfstate"
     encrypt = true
     region = "us-east-1"
